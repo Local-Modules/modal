68576a3a3ceb9d80d2628d57c95de9a94a0a0d50
Stas Onatskiy <code@onatsky.ru>
2023-06-20 13:25:37 +0300

    add useModalOpenState hook to have a control of specific modal state outside modal itself

---
+++ src/hooks/useModalOpenState.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react'
+import { manager, type ModalName } from 'src/manager'
+
+
+export default function useModalOpenState(name: ModalName) {
+  const [ isOpen, setIsOpen ] = useState(false)
+
+  useEffect(() => {
+    const listener = (eventModalName: ModalName) => {
+      if (eventModalName !== name) {
+        return
+      }
+
+      setIsOpen(manager.getOpenModalNames().includes(name))
+    }
+
+    manager.subscribe(listener)
+
+    return () => {
+      manager.unsubscribe(listener)
+    }
+  }, [ name ])
+
+  return isOpen
+}
